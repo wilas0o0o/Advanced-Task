326bdb10395a32ae95d09995a49da05816dccee0
wilas0o0o <03mcu30@gmail.com>
2021-12-16 12:10:05 +0900

    [解答]応用課題7a

---
+++ advanced_problem_7a-main/app/models/book.rb
@@ -0,0 +1,12 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+end
