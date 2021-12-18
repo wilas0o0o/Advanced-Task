2869ceb2a73dc07270311eb124c6b0ce22cd219f
wilas0o0o <03mcu30@gmail.com>
2021-12-18 21:22:15 +0900

    [解答]応用課題8b

---
+++ advanced_problem_8b/app/models/book.rb
@@ -0,0 +1,19 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) } # 2日前
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) } # 3日前
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) } # 4日前
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) } # 6日前
+end
